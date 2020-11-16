1f76bf31f06039e849eaec99195d07ae3899515c
Kamil Ismayil <kamil.ismayil@momentum-industrial.com>
2020-11-17 00:40:59 +0100

    committing user login

--- src/Context/login-context.js
+++ src/Context/login-context.js
@@ -1,4 +1,6 @@
-import React, {Component, useState} from 'react';
+import React, {useCallback, useEffect, useState} from 'react';
+import useHttp from '../Hooks/http';
+import axios from 'axios';
 
 export const LoginContext = React.createContext({
     isLogged: false,
@@ -9,16 +11,30 @@ export const LoginContext = React.createContext({
 const loginContextProvider = (props) => {
 
     const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const {isLoading, error, data, sendRequest} = useHttp(); 
 
-    const loginHandler = (username, password) => {
-        if(username === 'kamil' && password === 'kamil'){
+    const loginHandler = useCallback( (username, password) => {
+        sendRequest(
+            'https://hobbyproject-2020.herokuapp.com/api/login',
+            'GET', 
+            {username: username, password: password}
+        );
+    }, [sendRequest]); 
+
+    useEffect(() => {
+        if(data){
+            //console.log(data.firstName);
             setIsLoggedIn(true);
         }
-    }
+    },[data]);
 
     const logoutHandler = () =>{
         setIsLoggedIn(false);
     }
+
+    useEffect(() =>{
+        
+    } );
 
     return (
         <LoginContext.Provider value={{isLogged: isLoggedIn, login: loginHandler,logout: logoutHandler}}>
---
+++ src/Hooks/http.js
@@ -0,0 +1,47 @@
+import {useReducer, useCallback} from 'react';
+import axios from 'axios';
+
+const initialState = {
+    error: null, 
+    data: null, 
+    loading: false
+}
+
+const httpReducer = (currHttpState, action) => {
+    switch(action.type){
+        case 'SEND':
+          return {loading: true, error: null, data: null};
+        case 'RESPONSE':
+          return {...currHttpState, loading: false, data: action.responseData}; 
+        case 'ERROR':
+            return {loading: false, error: action.errorMessage};
+        default: 
+          throw new Error('Should not be reached!');
+      }
+}
+
+const useHttp = () => {
+    const [httpState, dispatchHttp] = useReducer(httpReducer, initialState);
+    
+
+    const sendRequest = useCallback( (url, method, body) => {  
+        dispatchHttp({type: 'SEND'});
+        let headers = {'Content-type': 'application/json', 'Accept': 'application/json'};    
+                
+        //axios.get(url, { method: method, body: body,
+        //        headers: {'Content-type': 'application/json', 'Accept': 'application/json'}})
+        axios.post(url, body, headers)
+        .then(response => { dispatchHttp({type:'RESPONSE', responseData: response.data}); })
+        .catch(error => {{ dispatchHttp({type: 'ERROR', errorMessage: 'Something went wrong'}); }});
+    }, []);
+
+    return {
+        isLoading: httpState.loading,
+        data: httpState.data,
+        error: httpState.error, 
+        sendRequest: sendRequest
+    };
+
+}
+
+export default useHttp;
